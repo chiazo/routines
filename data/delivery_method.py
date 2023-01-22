95b7ce12188c008e89906855a8aa9e03a94261bd
Chiazo Agina <chiazo@alumni.unc.edu>
2023-01-22 12:24:55 -0500

    get texts to send

--- data/delivery_method.py
+++ data/delivery_method.py
@@ -1,10 +1,10 @@
-from email.message import EmailMessage
 import os
 from os import environ as env
 from dotenv import load_dotenv
 from datetime import datetime
 from operator import itemgetter
-from .PyMessenger import Email, SMS, Messenger
+from .PyMessenger import Email, Messenger
+from etext import send_sms_via_email
 
 
 class DeliveryMethod:
@@ -32,8 +32,8 @@ class DeliveryMethod:
         load_dotenv(
             f"{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}/.env")
         date = self.msg_next_date.strftime('%a %b %d %Y')
-        subject = f'You have a reminder for {date}!'
-        message = f"Hi! Here's your reminder that there's {self.msg_cadence.value[1]} day(s) until you should {self.msg_name}. Upcoming date: {date}"
+        subject = f'[Reminder] For {date}!'
+        message = f"Hi! { self.msg_cadence.value[1] } day(s) until you should {self.msg_name}.\nUpcoming: {date}"
 
         if self.delivery_method == "Phone":
             self.send_text(message, subject)
@@ -41,20 +41,14 @@ class DeliveryMethod:
             self.send_email(message, subject)
 
     def send_text(self, content, subject):
-        my_messenger = Messenger(env['GMAIL_USER'], env['GMAIL_PASSWORD'])
+        sender_credentials = (env['GMAIL_USER'], env['GMAIL_PASSWORD'])
 
-        # Build the message
-        number = str(self.phone)
-        gateway = DeliveryMethod.carriers[self.carrier]
-        msg = SMS(number, gateway, subject, content)
-
-        # Send the message
-        my_messenger.send_sms(msg, one_time=True)
-        return None
+        return send_sms_via_email(
+            self.phone, content, self.carrier, sender_credentials, subject=subject
+        )
 
     def send_email(self, content, subject):
         my_messenger = Messenger(env['GMAIL_USER'], env['GMAIL_PASSWORD'])
         to = self.email
         msg = Email(to, subject, content, is_HTML=False)
-        my_messenger.send_email(msg, one_time=True)
-        return None
+        return my_messenger.send_email(msg, one_time=True)
